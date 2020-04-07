d0a53bb72ded64e0262a4ab29e99b64f3463766e
Davide <davide.roberti89@gmail.com>
2020-04-07 16:47:54 +0200

    creazione grafico

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
 
     var oggettoIntermedio = {};
-    console.log(oggettoIntermedio);
+    // console.log(oggettoIntermedio);
 
     var labelsMese = [];
     var valoriMese = [];
 
-    console.log(labelsMese);
-    console.log(valoriMese);
+    // console.log(labelsMese);
+    // console.log(valoriMese);
+
+    var ctx = $('#grafico');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+                labels: labelsMese,
+                datasets: [{
+                label: 'Period 1',
+                backgroundColor: 'rgba(0,0,0,0)',
+                borderColor: 'blue',
+                lineTension: 0,
+                data: valoriMese
+            }]
+        }
+    });
 
     $.ajax({
         url: 'http://157.230.17.132:4031/sales',
