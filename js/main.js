19ea108db0bde83e6de70522ccac70ec6f8fdca8
Davide <davide.roberti89@gmail.com>
2020-04-07 16:27:35 +0200

    creazione due  array per mesi e valori

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-    // http://157.230.17.132:4031/sales
 
     var oggettoIntermedio = {};
+    console.log(oggettoIntermedio);
+
+    var labelsMese = [];
+    var valoriMese = [];
+
+    console.log(labelsMese);
+    console.log(valoriMese);
 
     $.ajax({
         url: 'http://157.230.17.132:4031/sales',
@@ -11,13 +17,18 @@ $(document).ready(function(){
             for (var i = 0; i < infoVenditore.length; i++) {
                 var venditoreSingolo = infoVenditore[i];
                 var dataVendite = venditoreSingolo.date;
-                var datazione = moment(dataVendite, "DD-MM-YYYY").month();
-                console.log(datazione);
+                var datazioneMese = moment(dataVendite, "DD-MM-YYYY").format('MMMM');
+                if (oggettoIntermedio[datazioneMese] === undefined) {
+                    oggettoIntermedio[datazioneMese] = 0;
+                }
+                oggettoIntermedio[datazioneMese] += venditoreSingolo.amount;
             }
-
+            for (var key in oggettoIntermedio) {
+                labelsMese.push(key);
+                valoriMese.push(oggettoIntermedio[key]);
+            }
         },
         error: function(){
-
         }
     });
 });
