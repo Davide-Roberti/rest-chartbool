308937bde5652e84c7c7b9ce791cb930d2472321
Davide <davide.roberti89@gmail.com>
2020-04-07 15:55:54 +0200

    estrazione mesi

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-    
+    // http://157.230.17.132:4031/sales
+
+    var oggettoIntermedio = {};
+
+    $.ajax({
+        url: 'http://157.230.17.132:4031/sales',
+        method: 'GET',
+        success: function(data){
+            var infoVenditore = data;
+            for (var i = 0; i < infoVenditore.length; i++) {
+                var venditoreSingolo = infoVenditore[i];
+                var dataVendite = venditoreSingolo.date;
+                var datazione = moment(dataVendite, "DD-MM-YYYY").month();
+                console.log(datazione);
+            }
+
+        },
+        error: function(){
+
+        }
+    });
 });
